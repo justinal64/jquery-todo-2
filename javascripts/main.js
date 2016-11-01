547294f623804a0c4a98999c400ffdd3922b43d7
Zoe Ames <zoeames@gmail.com>
2016-10-31 19:42:54 -0500

    GET working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,15 @@
 "use strict";
 
+  let apiKeys = {};
+
 $(document).ready(function(){
   FbAPI.firebaseCredentials().then(function(keys){
     console.log("keys", keys);
+    apiKeys = keys;
+    firebase.initializeApp(apiKeys);
+    FbAPI.getTodos(apiKeys).then(function(items){
+      console.log("items from FB", items);
+    });
   });
 
 
